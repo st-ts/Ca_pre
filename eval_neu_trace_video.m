91b4bd1fc6ac730ebab9195a29631dbc1fc087e6
st-ts <stetsenko.an@gmail.com>
2023-11-28 07:51:10 -0500

    video

--- eval_neu_trace_video.m
+++ eval_neu_trace_video.m
@@ -59,9 +59,10 @@ while choice == "No"
     choice = questdlg('Are you happy with the drawn shape?', 'Confirmation', "Yes", "No", "Yes");
 end
 close(12);
-
+% neu_data=reg;
 
 %%
+n_frames_vid = 2000;
 for neu = 1:total_n_neu
     % Make a figure with 1 long subplot on the top with neuronal trace, and
     % 2 small subplots below: close up video of a neuron's activity & a pic
@@ -77,8 +78,14 @@ for neu = 1:total_n_neu
         figure(11); title(['Neuron #' num2str(neu) ' of ' num2str(total_n_neu)])
         % Calcium signal trace
         subplot(2, 2, [1 2]);
-        plot(data(neu,:)); xlim([0 size(data,2)]);
-    
+        trace = data(neu,:);
+        [max_val, max_fn] = max(trace(1:end-n_frames_vid));
+        plot(trace); xlim([0 size(data,2)]);
+        pause(0.3);
+                % Wait for the user to click on the first graph
+    fprintf('Click on the first graph to select a point.\n');
+    [t0, ~] = ginput(1);  % Indicate the 1st frame
+    [t_end, ~] = ginput(1);  % Indicate the 1st frame
         % The position of the neuron on top of the whole window
         subplot(2,2,3);
         imshow(imaxn); hold on;
@@ -86,8 +93,37 @@ for neu = 1:total_n_neu
         plot(neu_x,neu_y, 'ro', 'MarkerFaceColor', 'none', 'MarkerSize', 7, ...
             'LineWidth', 1.5);
     
-        pause(.5);
+
+
+            pause(.3);
+        subplot(2,2,4);
+        neu_data = reg(neu_x-7:neu_x+7,neu_y-7:neu_y+7,t0:t_end);
+        if max_fn<200
+            max_fn =201;
+        end
+        play_ca_im_vid(neu_data,10);
         %scrollplot('Axis', 'X'); 
+
+        % 2 little videos (2 minutes at 5x speed of the neuron around the max activity
+       %  for fn = 1:speed:size(neu_data,3)
+       %  % d
+       %  if istif
+       %      %figure(tiffig);
+       %      subplot(1,2,1);
+       %      neu_datta = imread(file_tif, 'Index', fn);
+       %      imshow(neu_datta, [], 'InitialMagnification', 1000); colormap(gray); hold on;
+       %  % imshow(neu_data(:,:,fn), 'InitialMagnification', 1000); hold on;
+       % plot(neu_cols,neu_rows, 'ro', 'MarkerFaceColor', 'none', 'MarkerSize', 6);
+       %  end
+       % 
+       %  subplot(1,2,2);
+       %  imshow(neu_data(:,:,fn), 'InitialMagnification', 1000); hold on;
+       %  plot(neu_cols,neu_rows, 'ro', 'MarkerFaceColor', 'none', 'MarkerSize', 6);
+       %  pause(0.05)
+       %  end
+
+
+        % categorize the neuron based on user input
         nq = input(['Quality of neuron #' num2str(neu) ' of ' num2str(size(data, 1)) ...
             ': (0 - not a neuron, 1(.) - terrible, 2(;) - good, 3(") - great:\n'], "s");
         if isempty(nq)
